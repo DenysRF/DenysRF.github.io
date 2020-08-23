d714a532317068ac50cf0d9f9511b715bc1f3c76
DenysRF <denysflederus@gmail.com>
2020-08-23 16:21:28 +0200

    illustration pages added

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+$('#hamburger').load('blocks/sidebar.html');
+$('#sidebar').load('blocks/sidebar.html');
+
+function fill(item, index) {
+    var col;
+    if (index < left) {
+        col = "images_left";
+    } else {
+        col = "images_right";
+    }
+    
+    document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
+}
