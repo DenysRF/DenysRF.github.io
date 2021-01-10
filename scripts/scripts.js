77b90982a752a51ab4509ab577ed86133181d03a
DenysRF <denysflederus@gmail.com>
2021-01-10 23:02:53 +0100

    script fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ function fill(item, index) {
     if (index < left) {
     	col = "images";
     } else {
-    	col = "images_right";
+    	col = "images-right";
     }   
     document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
 }
