360b714fe6106025d0ff161369409f449537f62a
DenysRF <denysflederus@gmail.com>
2021-01-10 22:59:38 +0100

    home

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,9 @@
 function fill(item, index) {
-    var col = "images";    
+    var col = "images";
+    if (index < left) {
+    	col = "images";
+    } else {
+    	col = "images_right";
+    }   
     document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
 }
