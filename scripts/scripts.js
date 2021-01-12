694b39215b5a3a9737206547b77445476853624d
DenysRF <denysflederus@gmail.com>
2021-01-12 19:58:40 +0100

    img sizes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,3 @@
 function fill(item, index) {
-    var col = "images"; 
-    document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
-}
-
-function fill_by_name(item, index, name) {
-    var col = name; 
-    document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
+    document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" class=\"" + size[index] + "\"></div><p>" + text[index] + "</p>";
 }
