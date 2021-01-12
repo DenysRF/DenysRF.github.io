693bbfc7a1415765c6787ff87a71841975ade24c
DenysRF <denysflederus@gmail.com>
2021-01-12 18:58:51 +0100

    printing tech
    toto home page buttons for these and scroll to location

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,8 @@ function fill(item, index) {
     var col = "images"; 
     document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
 }
+
+function fill_by_name(item, index, name) {
+    var col = name; 
+    document.getElementById(col).innerHTML += "<img src=\"" + path + item + ".jpg\" class=\"img-fluid\"><p>" + text[index] + "</p>";
+}
