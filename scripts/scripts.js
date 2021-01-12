aa15d02675a422cd772f9fb6c2bb105d71f7ac62
DenysRF <denysflederus@gmail.com>
2021-01-12 22:12:10 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 function fill(item, index) {
-    document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" class=\"" + size[index] + "\"></div><p>" + text[index] + "</p>";
+    document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" loading=\"lazy\" class=\"" + size[index] + "\"></div><p>" + text[index] + "</p>";
 }
 
 function fill_text_center(item, index) {
-    document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" class=\"" + size[index] + "\"></div><div class=\"text-center\"><p>" + text[index] + "</p></div>";
+    document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" loading=\"lazy\" class=\"" + size[index] + "\"></div><div class=\"text-center\"><p>" + text[index] + "</p></div>";
 }
