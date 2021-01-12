b59e3f7d809e67cbcdb372016a007fd6a63c0235
DenysRF <denysflederus@gmail.com>
2021-01-12 20:35:58 +0100

    done?

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
 function fill(item, index) {
     document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" class=\"" + size[index] + "\"></div><p>" + text[index] + "</p>";
 }
+
+function fill_text_center(item, index) {
+    document.getElementById(col).innerHTML += "<div class=\"text-center\"><img src=\"" + path + item + ".jpg\" class=\"" + size[index] + "\"></div><div class=\"text-center\"><p>" + text[index] + "</p></div>";
+}
